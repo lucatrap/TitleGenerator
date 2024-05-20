8e66dacbe0a78d8a74fc3ba74cdda21f87aa6785
Luca Trapani <luca.t.trapani@gmail.com>
2024-05-20 12:12:56 +0200

    Add Draw method
    Draw method allows to print title in the console.

--- TitleGenerator/Program.cs
+++ TitleGenerator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using TitleGenerator;
 
-Title title = new(["Hello", "World", "\t"], 100, '*', 1, 1);
+Title title = new(["Hello", "World", "!"], 100, '*', 1, 1);
 
-Console.WriteLine(title.Text[0]);
-Console.WriteLine(title.Pattern);
+title.Draw();
--- TitleGenerator/Title.cs
+++ TitleGenerator/Title.cs
@@ -20,5 +20,31 @@ public class Title
     }
 
     // Methods
+    public void Draw()
+    {
+        for (int i = 0; i < Border; i++)
+        {
+            Console.WriteLine(new string(Pattern, Width));
+        }
 
+        for (int i = 0; i < Padding; i++)
+        {
+            Console.WriteLine(new string(Pattern, Border) + new string(' ', Width - Border - Border) + new string(Pattern, Border));
+        }
+
+        foreach (string line in Text)
+        {
+            Console.WriteLine(new string(Pattern, Border) + new string(' ', (Width - Border - Border - line.Length) / 2) + line + new string(' ', (Width - Border - Border - line.Length) / 2) + new string(Pattern, Border));
+        }
+
+        for (int i = 0; i < Padding; i++)
+        {
+            Console.WriteLine(new string(Pattern, Border) + new string(' ', Width - Border - Border) + new string(Pattern, Border));
+        }
+
+        for (int i = 0; i < Border; i++)
+        {
+            Console.WriteLine(new string(Pattern, Width));
+        }     
+    }
 }
