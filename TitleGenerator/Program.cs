21615637d4cc10ae3cbf1acdc10f5c443c1e5e31
Luca Trapani <luca.t.trapani@gmail.com>
2024-05-20 11:51:36 +0200

    Add basic properties to Title class
    The properties added include title text, width, padding, border width, and pattern used for border.

--- TitleGenerator/Program.cs
+++ TitleGenerator/Program.cs
@@ -1,1 +1,6 @@
 ﻿using TitleGenerator;
+
+Title title = new(["Hello", "World", "\t"], 100, '*', 1, 1);
+
+Console.WriteLine(title.Text[0]);
+Console.WriteLine(title.Pattern);
--- TitleGenerator/Title.cs
+++ TitleGenerator/Title.cs
@@ -2,5 +2,23 @@ namespace TitleGenerator;
 
 public class Title
 {
-    
+    // Properties
+    public string[] Text { get; set; }
+    public int Width { get; set; }
+    public char Pattern { get; set; }
+    public int Padding { get; set; }
+    public int Border { get; set; }
+
+    // Constructor
+    public Title(string[] text, int width, char pattern, int padding, int border)
+    {
+        Text = text;
+        Width = width;
+        Pattern = pattern;
+        Padding = padding;
+        Border = border;
+    }
+
+    // Methods
+
 }
